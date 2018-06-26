2ebddd0ebbc021943a8822f7e352ba53e6cb1c70
Robert <robert.premar@gmail.com>
2018-06-27 00:10:59 +0200

    Replacing ifconfig in favor of ip
    Fixes #5

--- src/dokman/importDockerHostIP.sh
+++ src/dokman/importDockerHostIP.sh
@@ -11,7 +11,7 @@ function importDockerHostIP
     hostIp=""
 
     if isLinux; then
-        hostIp=$(ifconfig | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk '{ print $2 }' | cut -f2 -d: | head -n1)
+        hostIp=$(ip -4 addr show docker0 | grep -Po 'inet \K([0-9]{1,3}\.){3}[0-9]{1,3}' --color=never)
     fi
 
     export DOKMAN_HOST_IP="${hostIp}"
