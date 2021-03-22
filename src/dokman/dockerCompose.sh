4cab260bea58a08e0ed970420a900b13aeef8940
Robert Premar <robert.premar@gmail.com>
2021-03-22 12:22:32 +0100

    Env variables in .install.sh file
    All environment variables defined in .env and .env.dist, no
    matter docker root or environemnt folder where not loaded
    in instalation script. Now those environemnt variables are
    avaliable in install script.

--- src/dokman/dockerCompose.sh
+++ src/dokman/dockerCompose.sh
@@ -16,37 +16,10 @@ function dockerCompose
     local arguments=("${@:4}")
 
     local environmentPath
-    local projectNamePath
 
     environmentPath=$(environmentFilePath "${path}" "${env}")
-    projectNamePath=$(projectNameCacheFilePath "${dokmanDir}")
 
-    if ! hasEnv "${path}" "${env}" ; then
-        error "Environment $(foregroundColor "${env}" "yellow") is not defined in environments directory"
-        exit 1
-    fi
-
-    # import current user and group
-    importCurrentUserGroup
-
-    # import docker host IP
-    importDockerHostIP
-
-    if [ ! -f "${path}/.env.dist" ]; then
-        error "Missing main $(foregroundColor ".env.dist" "yellow") file"
-        exit 1
-    fi
-
-    # import .env.dist and .env files
-    importConfigurations "${path}"
-    # import env specific config if needed
-    importConfigurations "${environmentPath}"
-
-    handleProjectName "${projectNamePath}"
-    if ! importProjectName "${projectNamePath}" ; then
-        error "Variable $(foregroundColor "COMPOSE_PROJECT_NAME" "yellow") missing, please add at least default one"
-        exit 1
-    fi
+    loadEnvFiles "${dokmanDir}" "${path}" "${env}"
 
     local command="${arguments[0]}"
     local currentlyRunning=0
